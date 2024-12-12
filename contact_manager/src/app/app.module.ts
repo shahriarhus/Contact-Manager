5692751ac27d647809ae7338052a1ac2cff6b878
Shahriar Hussain <shahriarhus@gmail.com>
2024-12-12 11:41:47 +0530

    Created components

--- contact_manager/src/app/app.module.ts
+++ contact_manager/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ViewContactComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ contact_manager/src/app/view-contact/view-contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-contact',
+  templateUrl: './view-contact.component.html',
+  styleUrl: './view-contact.component.css'
+})
+export class ViewContactComponent {
+
+}
