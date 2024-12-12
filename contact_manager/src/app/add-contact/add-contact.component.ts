cd6fdb4128b0868a65984b0dd14e26ee291d9a18
Shahriar Hussain <shahriarhus@gmail.com>
2024-12-12 12:09:36 +0530

    Update add-contact.component.ts

--- contact_manager/src/app/add-contact/add-contact.component.ts
+++ contact_manager/src/app/add-contact/add-contact.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ContactService } from '../contact.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
-  styleUrl: './add-contact.component.css'
+  styleUrls: ['./add-contact.component.css']
 })
-export class AddContactComponent {
+export class AddContactComponent implements OnInit {
+  contactForm!: FormGroup; // Use definite assignment assertion
 
+  constructor(
+    private fb: FormBuilder,
+    private contactService: ContactService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // Initialize the form in ngOnInit
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      number: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      this.contactService.addContact(this.contactForm.value).subscribe(() => {
+        this.router.navigate(['/dashboard']);
+      });
+    }
+  }
 }
